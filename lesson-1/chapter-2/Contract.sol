1c5fedebf4c81605a2d2f91a0243d71414d9dc36
krishna-github-metrostate <98346300+krishna-github-metrostate@users.noreply.github.com>
2022-04-01 11:44:48 -0500

    Update Contract.sol

--- lesson-1/chapter-2/Contract.sol
+++ lesson-1/chapter-2/Contract.sol
@@ -1,16 +1,5 @@
+pragma solidity >=0.5.0 <0.6.0;
 
-pragma solidity ^0.4.25;
+contract ZombieFactory {
 
-      contract ZombieFactory {
-
-          uint dnaDigits = 16;
-          uint dnaModulus = 10 ** dnaDigits;
-
-          struct Zombie {
-              string name;
-              uint dna;
-          }
-
-          Zombie[] public zombies;
-
-      }
+}
