a4f92ce3a73b82045d8d6e770f86099b133ce538
krishna-github-metrostate <98346300+krishna-github-metrostate@users.noreply.github.com>
2022-04-03 10:57:43 -0500

    Update zombiefeeding.sol

--- lesson-2/chapter-7/zombiefeeding.sol
+++ lesson-2/chapter-7/zombiefeeding.sol
@@ -1,5 +1,7 @@
-pragma solidity ^0.4.25;
+pragma solidity >=0.5.0 <0.6.0;
+
 import "./zombiefactory.sol";
+
 contract ZombieFeeding is ZombieFactory {
 
   function feedAndMultiply(uint _zombieId, uint _targetDna) public {
