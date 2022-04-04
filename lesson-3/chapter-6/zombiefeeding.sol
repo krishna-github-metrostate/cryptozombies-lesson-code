322c6cd945923d2c5d451d70b7245767d2d2005a
krishna-github-metrostate <98346300+krishna-github-metrostate@users.noreply.github.com>
2022-04-04 10:54:26 -0500

    Update zombiefeeding.sol

--- lesson-3/chapter-6/zombiefeeding.sol
+++ lesson-3/chapter-6/zombiefeeding.sol
@@ -1,5 +1,7 @@
-pragma solidity ^0.4.25;
+pragma solidity >=0.5.0 <0.6.0;
+
 import "./zombiefactory.sol";
+
 contract KittyInterface {
   function getKitty(uint256 _id) external view returns (
     bool isGestating,
@@ -14,6 +16,7 @@ contract KittyInterface {
     uint256 genes
   );
 }
+
 contract ZombieFeeding is ZombieFactory {
 
   KittyInterface kittyContract;
@@ -30,7 +33,7 @@ contract ZombieFeeding is ZombieFactory {
       return (_zombie.readyTime <= now);
   }
 
-  function feedAndMultiply(uint _zombieId, uint _targetDna, string _species) public {
+  function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) public {
     require(msg.sender == zombieToOwner[_zombieId]);
     Zombie storage myZombie = zombies[_zombieId];
     _targetDna = _targetDna % dnaModulus;
