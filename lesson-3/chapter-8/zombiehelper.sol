466dd8dd540c0f03221a3587b31ee4bef580d1d0
krishna-github-metrostate <98346300+krishna-github-metrostate@users.noreply.github.com>
2022-04-04 10:58:12 -0500

    Update zombiehelper.sol

--- lesson-3/chapter-8/zombiehelper.sol
+++ lesson-3/chapter-8/zombiehelper.sol
@@ -1,5 +1,7 @@
-pragma solidity ^0.4.25;
+pragma solidity >=0.5.0 <0.6.0;
+
 import "./zombiefeeding.sol";
+
 contract ZombieHelper is ZombieFeeding {
 
   modifier aboveLevel(uint _level, uint _zombieId) {
