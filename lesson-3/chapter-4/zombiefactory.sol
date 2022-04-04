19f49ce2349a9b0e6fa252dd2d882afff0346886
krishna-github-metrostate <98346300+krishna-github-metrostate@users.noreply.github.com>
2022-04-04 10:46:59 -0500

    Update zombiefactory.sol

--- lesson-3/chapter-4/zombiefactory.sol
+++ lesson-3/chapter-4/zombiefactory.sol
@@ -1,5 +1,7 @@
-pragma solidity ^0.4.25;
+pragma solidity >=0.5.0 <0.6.0;
+
 import "./ownable.sol";
+
 contract ZombieFactory is Ownable {
 
     event NewZombie(uint zombieId, string name, uint dna);
@@ -10,6 +12,7 @@ contract ZombieFactory is Ownable {
     struct Zombie {
         string name;
         uint dna;
+        
         uint32 level;
         uint32 readyTime;
     }
@@ -19,19 +22,19 @@ contract ZombieFactory is Ownable {
     mapping (uint => address) public zombieToOwner;
     mapping (address => uint) ownerZombieCount;
 
-    function _createZombie(string _name, uint _dna) internal {
+    function _createZombie(string memory _name, uint _dna) internal {
         uint id = zombies.push(Zombie(_name, _dna)) - 1;
         zombieToOwner[id] = msg.sender;
         ownerZombieCount[msg.sender]++;
         emit NewZombie(id, _name, _dna);
     }
 
-    function _generateRandomDna(string _str) private view returns (uint) {
+    function _generateRandomDna(string memory _str) private view returns (uint) {
         uint rand = uint(keccak256(abi.encodePacked(_str)));
         return rand % dnaModulus;
     }
 
-    function createRandomZombie(string _name) public {
+    function createRandomZombie(string memory _name) public {
         require(ownerZombieCount[msg.sender] == 0);
         uint randDna = _generateRandomDna(_name);
         randDna = randDna - randDna % 100;
