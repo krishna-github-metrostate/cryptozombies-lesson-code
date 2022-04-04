1fc3540e585c3eea352450cd4bd443b789e9a43a
krishna-github-metrostate <98346300+krishna-github-metrostate@users.noreply.github.com>
2022-04-04 10:59:46 -0500

    Update zombiefeeding.sol

--- lesson-3/chapter-9/zombiefeeding.sol
+++ lesson-3/chapter-9/zombiefeeding.sol
@@ -1,50 +1,22 @@
 pragma solidity >=0.5.0 <0.6.0;
 
-import "./zombiefactory.sol";
+import "./zombiefeeding.sol";
 
-contract KittyInterface {
-  function getKitty(uint256 _id) external view returns (
-    bool isGestating,
-    bool isReady,
-    uint256 cooldownIndex,
-    uint256 nextActionAt,
-    uint256 siringWithId,
-    uint256 birthTime,
-    uint256 matronId,
-    uint256 sireId,
-    uint256 generation,
-    uint256 genes
-  );
-}
+contract ZombieHelper is ZombieFeeding {
 
-contract ZombieFeeding is ZombieFactory {
-
-  KittyInterface kittyContract;
-
-  function setKittyContractAddress(address _address) external onlyOwner {
-    kittyContract = KittyInterface(_address);
+  modifier aboveLevel(uint _level, uint _zombieId) {
+    require(zombies[_zombieId].level >= _level);
+    _;
   }
 
-  function _triggerCooldown(Zombie storage _zombie) internal {
-    _zombie.readyTime = uint32(now + cooldownTime);
-  }
-
-  function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) public {
+  function changeName(uint _zombieId, string calldata _newName) external aboveLevel(2, _zombieId) {
     require(msg.sender == zombieToOwner[_zombieId]);
-    Zombie storage myZombie = zombies[_zombieId];
-    _targetDna = _targetDna % dnaModulus;
-    uint newDna = (myZombie.dna + _targetDna) / 2;
-    if (keccak256(abi.encodePacked(_species)) == keccak256(abi.encodePacked("kitty"))) {
-      newDna = newDna - newDna % 100 + 99;
-    }
-    _createZombie("NoName", newDna);
-    _triggerCooldown(myZombie);
+    zombies[_zombieId].name = _newName;
   }
 
-  function feedOnKitty(uint _zombieId, uint _kittyId) public {
-    uint kittyDna;
-    (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
-    feedAndMultiply(_zombieId, kittyDna, "kitty");
+  function changeDna(uint _zombieId, uint _newDna) external aboveLevel(20, _zombieId) {
+    require(msg.sender == zombieToOwner[_zombieId]);
+    zombies[_zombieId].dna = _newDna;
   }
 
 }
