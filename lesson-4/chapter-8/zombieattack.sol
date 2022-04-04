7e2618bdd25da622d642b3a1a6a778cc72a976a3
krishna-github-metrostate <98346300+krishna-github-metrostate@users.noreply.github.com>
2022-04-04 16:04:43 -0500

    Update zombieattack.sol

--- lesson-4/chapter-8/zombieattack.sol
+++ lesson-4/chapter-8/zombieattack.sol
@@ -1,5 +1,7 @@
-pragma solidity ^0.4.25;
+pragma solidity >=0.5.0 <0.6.0;
+
 import "./zombiehelper.sol";
+
 contract ZombieAttack is ZombieHelper {
   uint randNonce = 0;
   uint attackVictoryProbability = 70;
@@ -7,6 +9,7 @@ contract ZombieAttack is ZombieHelper {
   function randMod(uint _modulus) internal returns(uint) {
     randNonce++;
     return uint(keccak256(abi.encodePacked(now, msg.sender, randNonce))) % _modulus;
+
   }
 
   function attack(uint _zombieId, uint _targetId) external ownerOf(_zombieId) {
