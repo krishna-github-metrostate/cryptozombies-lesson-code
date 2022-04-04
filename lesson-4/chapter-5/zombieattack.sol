51e92d2010d61d25a724c4cace7ab9d356b019ac
krishna-github-metrostate <98346300+krishna-github-metrostate@users.noreply.github.com>
2022-04-04 12:41:18 -0500

    Update zombieattack.sol

--- lesson-4/chapter-5/zombieattack.sol
+++ lesson-4/chapter-5/zombieattack.sol
@@ -1,7 +1,10 @@
-pragma solidity ^0.4.25;
+pragma solidity >=0.5.0 <0.6.0;
+
 import "./zombiehelper.sol";
+
 contract ZombieAttack is ZombieHelper {
   uint randNonce = 0;
+  
   uint attackVictoryProbability = 70;
 
   function randMod(uint _modulus) internal returns(uint) {
