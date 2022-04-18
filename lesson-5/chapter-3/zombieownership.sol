9e8ebd5450f373aaf3d0ff30727ed973c0ac0827
krishna-github-metrostate <98346300+krishna-github-metrostate@users.noreply.github.com>
2022-04-18 14:14:08 -0500

    Update zombieownership.sol

--- lesson-5/chapter-3/zombieownership.sol
+++ lesson-5/chapter-3/zombieownership.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.25;
+pragma solidity >=0.5.0 <0.6.0;
 
 import "./zombieattack.sol";
 import "./erc721.sol";
@@ -6,10 +6,12 @@ import "./erc721.sol";
 contract ZombieOwnership is ZombieAttack, ERC721 {
 
   function balanceOf(address _owner) external view returns (uint256) {
+    
     return ownerZombieCount[_owner];
   }
 
   function ownerOf(uint256 _tokenId) external view returns (address) {
+    
     return zombieToOwner[_tokenId];
   }
 
